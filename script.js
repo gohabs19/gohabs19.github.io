93a91f0d2662e47bced52fa4135cf8d9a60e5dc6
gohabs19 <57967821+gohabs19@users.noreply.github.com>
2020-12-23 22:49:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,19 +4,19 @@ window.onload = displayData();
 
 function displayData() {
     alert("hi");
-    $.getJSON("http://api.weatherstack.com/current?access_key=0e283bebc652f3bf4bc6b1808ae76a85&query=Richmond Hill",
+    $.getJSON("https://api.weatherstack.com/current?access_key=0e283bebc652f3bf4bc6b1808ae76a85&query=Richmond Hill",
         function weatherData(data) {
             console.log(data);
             document.getElementById('RichmondHillTemp').innerHTML = data.current.temperature;
         }
     );
-    $.getJSON("http://api.weatherstack.com/current?access_key=0e283bebc652f3bf4bc6b1808ae76a85&query=Toronto",
+    $.getJSON("https://api.weatherstack.com/current?access_key=0e283bebc652f3bf4bc6b1808ae76a85&query=Toronto",
         function weatherData(data) {
             console.log(data);
             document.getElementById('TorontoTemp').innerHTML = data.current.temperature;
         }
     );
-    $.getJSON("http://newsapi.org/v2/top-headlines?country=us&apiKey=165d34c7f4a9485d9953e37954a4afbc",
+    $.getJSON("https://newsapi.org/v2/top-headlines?country=us&apiKey=165d34c7f4a9485d9953e37954a4afbc",
         function newsData(data) {
             console.log(data);
             for (counter = 0; counter < data.articles.length - 1; counter++) {
