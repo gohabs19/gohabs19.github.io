3ed5a3fafef81bbb8fc71fa82293810061423c95
gohabs19 <57967821+gohabs19@users.noreply.github.com>
2020-12-23 22:51:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@
 window.onload = displayData();
 
 function displayData() {
-    alert("hi");
     $.getJSON("https://api.weatherstack.com/current?access_key=0e283bebc652f3bf4bc6b1808ae76a85&query=Richmond Hill",
         function weatherData(data) {
             console.log(data);
